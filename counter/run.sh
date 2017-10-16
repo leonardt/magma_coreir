b1cbc4ee2fce88e6914d7fe45bfc8727e1affa36
Lenny Truong <lenny@cs.stanford.edu>
2017-10-16 10:28:31 -0700

    Update submodules

--- counter/run.sh
+++ counter/run.sh
@@ -6,3 +6,5 @@ export PYTHONPATH=$PWD/../magma:$PWD/../mantle:$PWD/../coreir:$PYTHONPATH
 ../magma/bin/magma -o coreir -m coreir counter.py
 ../test-vectors -i counter.py -o build/counter.vec -n 32
 ../coreir-test -i build/counter.json -t build/counter.vec
+../coreir/bin/coreir -i build/counter.json -o build/counter.fir
+../firrtl-interpreter/run-test-vec.sh -f build/counter.fir -tv build/counter.vec -so
--- setup.sh
+++ setup.sh
@@ -5,6 +5,13 @@ git submodule update
 
 cd coreir
 make build
+make coreir
+cd ..
+
+cd firrtl
+sbt compile
+sbt assembly
+sbt publish-local
 cd ..
 
 cd firrtl-interpreter
